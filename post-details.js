42bc4185ba830d94e2d1a988160d4de3a7601d78
TesliukK <teslukkirill@gmail.com>
2022-12-25 16:24:14 +0200

    comments

--- post-details.js
+++ post-details.js
@@ -1,5 +1,6 @@
 const url = new URL(location.href)
 const id = url.searchParams.get('id')
+
 fetch(`https://jsonplaceholder.typicode.com/posts/` + id)
     .then(value => value.json())
     .then(posts => {
@@ -8,32 +9,31 @@ fetch(`https://jsonplaceholder.typicode.com/posts/` + id)
         block.innerText = `Id: ${posts.id} 
         Title: ${posts.title}
         Body: ${posts.body}`
-        const apiURL ='https://jsonplaceholder.typicode.com/comments/'
-        const urlComm = new URL(location.href)
-        const comment = urlComm.searchParams.get('comment')
-        blockBtn = document.createElement('div')
-        blockBtn.classList.add('blockBtn')
-        const button = document.createElement('button')
-        button.classList.add('button')
-        blockBtn.appendChild(button)
-        button.innerText = 'post of current user'
-        block.appendChild(blockBtn)
-        button.onclick = function () {
-            fetch(apiURL+comment)
-                .then(value => value.json())
-                .then(comments => {
-
-                    for (const commKey in comments) {
-                        for (const commKeyElement of commKey) {
-                            const comm = document.createElement('div')
-                            comm.innerText = `${commKey}`
-                            blockBtn.appendChild(comm)
-                            console.log(commKey);
-                        }
-                    }
-                })
-
-
-        }
-
     })
+
+fetch(`https://jsonplaceholder.typicode.com/comments`)
+    .then(value => value.json())
+    .then(comments => {
+        for (const comment of comments) {
+            const div = document.createElement('div')
+            div.innerText = `${comment.body}`
+            document.body.appendChild(div)
+        }
+    })
+
+// blockBtn = document.createElement('div')
+// blockBtn.classList.add('blockBtn')
+// document.body.appendChild(blockBtn)
+// const button = document.createElement('button')
+// button.classList.add('button')
+// blockBtn.appendChild(button)
+// button.innerText = 'post of current user'
+// button.onclick = function () {
+//     fetch(apiURL+comment)
+//         .then(value => value.json())
+//         .then(comments => {
+//
+//         })
+//
+//
+// }
