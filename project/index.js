5a77f77e921557f248413db60b745047b9ef7da3
TesliukK <teslukkirill@gmail.com>
2023-02-07 18:37:35 +0200

    add css

--- project/index.js
+++ project/index.js
@@ -6,9 +6,13 @@ fetch('https://jsonplaceholder.typicode.com/users')
         document.body.appendChild(container)
 
         for (const user of users) {
+            const block = document.createElement('div')
+            block.classList.add('block')
+
             const divUsers = document.createElement('div')
             divUsers.classList.add('divUsers')
             const h1Id = document.createElement('h2')
+            h1Id.classList.add('h1Id')
             h1Id.innerHTML = `<b>User - ${user.id}</b>`
             const name = document.createElement('h2')
             name.innerHTML = `<b>${user.name}</b>`
@@ -20,9 +24,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
             button.classList.add('button')
             button.innerText = 'Details of current user'
 
+            block.appendChild(divUsers)
             a.appendChild(button)
             divUsers.append(h1Id, name, a)
-            container.append(divUsers)
+            container.append(block)
 
         }
     })
